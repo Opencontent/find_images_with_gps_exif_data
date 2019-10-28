3d3f57d1514bca2bf0c14ec819d19b8d2f8d350d
root <root@nginx.opencontent.it>
2019-10-28 12:21:16 +0100

    adding script to remove GPS exif data

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # find_images_with_gps_exif_data.sh
 
-Search GPS info in images in a directory (recursively) 
+Search GPS info in images in a directory (recursively), and remove GPS data 
 
 Useful for privacy issue, to be sure a storage does not have GPS info saved in image file.
 
 
 Execute:
 
-find_images_with_gps_exif_data.sh mydirectory
+* find_images_with_gps_exif_data.sh mydirectory
+* remove_gps_exif_data_from_images.sh mydirectory
---
+++ remove_gps_exif_data_from_images.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# 
+# Search GPS info in images in a directory (recursively) and remove it - useful for privacy issue
+#  https://github.com/Opencontent/find_images_with_gps_exif_data
+# 
+#  prerequisite: ImageMagick, exiftool
+# 
+# Exit code
+#     1 : found images with GPS info
+#     0 : no images with GPS info found
+
+IDENTIFY_OUTPUT_TO_IGNORE="exif:GPSInfo\|exif:GPSVersionID"
+
+IDENTIFY=`which identify`
+if ! [ $? -eq 0 ]
+then
+   echo "Error: the command 'identify' from ImageMagick is not installed (yum install ImageMagick)"
+   exit 1
+fi
+
+EXIFTOOL=`which exiftool`
+if ! [ $? -eq 0 ]
+then
+   echo "Error: the command 'exiftool' is not installed (yum install perl-Image-ExifTool)"
+   exit 1
+fi
+
+
+DIR=$1
+this_script=`basename "$0"`
+
+
+if [[ "$DIR" == "" ]]
+then
+   echo "Error: missing parameter."
+   echo "Type:"
+   echo "./${this_script} DIRECTORY"
+   exit 1
+fi
+
+if ! [ $# -eq 1 ]
+then
+   echo "Error: only one parameter is accepted"
+   exit 1
+fi
+
+
+# list all images
+# we identify images by having string  "image data" in output of 'file' command (we also include files with that string in name file, but is not an issue)
+all_images=`find $DIR -type f | xargs -n1 file  | grep " image data" | awk -F':' '{ print $1}'`
+
+# test each image for GPS info
+total_numeber_of_images=`echo $all_images| xargs -n1 | wc -l`
+echo "Found $total_numeber_of_images images while searching recursively in directory: $DIR"
+if  [[ "$total_numeber_of_images" -lt 1 ]];
+then
+  exit
+else 
+  echo "Scanning each image for GPS info..."
+  counter=0
+  for image_file in $all_images
+  do
+    output=`$IDENTIFY -format "%[EXIF:*GPS*]" $image_file | grep -v -e '^$' | grep -v $IDENTIFY_OUTPUT_TO_IGNORE`
+    if ! [[ "$output" == "" ]]
+    then
+      echo "Found GPS info in image: $image_file"
+      counter=$((counter+1))
+      echo "Removing gps data from image: $image_file"
+      $EXIFTOOL -gps:all= -xmp:geotag= -overwrite_original_in_place $image_file
+    fi
+  done
+
+  if [[ $counter == "0" ]]
+  then
+    echo "No images with GPS info found"
+  else 
+    echo "Found a Total of $counter images with GPS info"
+    exit 1
+  fi
+fi
